6050a4b13b71e1cba0cf7c6a5f5bb7c8631ad6cb
NayCS <nali.pei.smile@gmail.com>
2019-11-10 08:34:44 -0500

    add routes and server

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+let starWarsFriendsArray =
+    [
+        {
+            "name": "Star Wars Robot",
+            "photo": "https://owips.com/sites/default/files/clipart/toons-clipart/158142/toons-clipart-star-wars-158142-8422434.jpg",
+            "scores": [
+                "5",
+                "1",
+                "4",
+                "4",
+                "5",
+                "1",
+                "2",
+                "5",
+                "4",
+                "1"
+            ]
+        },
+        {
+            "name": "Star Wars Darth Vader",
+            "photo": "https://d2tml28x3t0b85.cloudfront.net/tracks/artworks/000/147/864/original/d6780b.jpeg?1460063548",
+            "scores": [
+                "4",
+                "2",
+                "5",
+                "1",
+                "3",
+                "2",
+                "2",
+                "1",
+                "3",
+                "2"
+            ]
+        },
+        {
+            "name": "Star Wars Yoda",
+            "photo": "https://i.dlpng.com/static/png/404285_preview.png",
+            "scores": [
+                "5",
+                "2",
+                "2",
+                "2",
+                "4",
+                "1",
+                "3",
+                "2",
+                "5",
+                "5"
+            ]
+        },
+        {
+            "name": "Star Wars Obi-Wan Kenobi",
+            "photo": "https://i1.wp.com/movienetworkpr.com/wp-content/uploads/2019/11/OBI-WAN-KENOBI.jpg?resize=759%2C500&ssl=1",
+            "scores": [
+                "3",
+                "3",
+                "4",
+                "2",
+                "2",
+                "1",
+                "3",
+                "2",
+                "2",
+                "3"
+            ]
+        },
+        {
+            "name": "Star Wars Clon",
+            "photo": "https://www.pnglot.com/pngfile/detail/103-1035158_clon-star-wars-png-clone-trooper-phase-1.png",
+            "scores": [
+                "4",
+                "3",
+                "4",
+                "1",
+                "5",
+                "2",
+                "5",
+                "3",
+                "1",
+                "4"
+            ]
+        },
+        {
+            "name": "Star Wars C-3 Po",
+            "photo": "https://www.pnglot.com/pngfile/detail/244-2445040_c-3po-star-wars-clone-wars-c3po.png",
+            "scores": [
+                "4",
+                "4",
+                "2",
+                "3",
+                "2",
+                "2",
+                "3",
+                "2",
+                "4",
+                "5"
+            ]
+        },
+        {
+            "name": "Star Wars Alien Padawan",
+            "photo": "https://www.netclipart.com/pp/m/293-2939089_star-wars-alien-padawan.png",
+            "scores": [
+                "2",
+                "2",
+                "1",
+                "3",
+                "1",
+                "1",
+                "3",
+                "1",
+                "1",
+                "1"
+            ]
+        },
+        {
+            "name": "Star Wars Rei",
+            "photo": "https://www.picclickimg.com/d/l400/pict/121750608961_/REY-Star-Wars-VII-Force-Awakens-Daisy-Ridley.jpg",
+            "scores": [
+                "3",
+                "4",
+                "2",
+                "3",
+                "3",
+                "1",
+                "5",
+                "2",
+                "2",
+                "3"
+            ]
+        },
+        {
+            "name": "Star Wars Be Mine",
+            "photo": "https://hbdchick.files.wordpress.com/2015/02/star-wars-legos-valentines-day-04.jpgtest picture ",
+            "scores": [
+                "5",
+                "4",
+                "2",
+                "1",
+                "4",
+                "4",
+                "5",
+                "5",
+                "2",
+                "2"
+            ]
+        }
+    ]
+
+module.exports = starWarsFriendsArray;
